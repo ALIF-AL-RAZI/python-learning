46433353545ae0f296681d99f85a52334fc50a83
ALIF-AL-RAZI <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2022-09-15 13:09:10 +0600

    Add files via upload

---
+++ series_practice.py
@@ -0,0 +1,55 @@
+#1+2+3+4+5+....+n
+
+'''n= int(input("Enter the last number: "))
+
+s1=0
+
+for x in range(1,n+1,1):
+    s1 =s1+x
+print(s1)'''
+
+#2+4+6+8+...2n
+
+'''n=int(input("enter the last number:"))
+sum =0
+for x in range(2,n+1,2):
+    sum=sum +x
+print(sum)'''
+
+#1+3+5+...+n
+
+'''n=int(input("enter the last number: "))
+sum=0
+for x in range(1,n+1,2):
+    sum=sum+x
+print(sum)'''
+
+#1**2+2**2+3**2+4**2+...n**2
+
+'''n= int (input("enter the last number: "))
+sum=0
+for x in range(1, (n+1), 1):
+    sum=sum+x**2
+print(sum)'''
+
+#1**(1/2) + 2**(1/2) + 3**(1/2) + 4**(1/2) + ... + n**(1/2)
+
+n =int(input("enter n:"))
+sum=0
+for x in range(1, n+1, 1):
+    sum=sum+x **(1/2)
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
