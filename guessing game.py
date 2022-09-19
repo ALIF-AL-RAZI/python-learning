292c5c13a7a8494e9d5d0df432cb52e71b292e2b
ALIF-AL-RAZI <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2022-09-19 14:18:20 +0600

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -2,14 +2,17 @@ import random
 
 #guessNumber = int(input("Enter your guess between 1 to 10: "))
 randomNumber = random.randint(1,10)
-
+x=0
+# print(randomNumber)
 while(True):
     guessNumber = int(input("Enter your guess between 1 to 10: "))
 
-    if guessNumber == randomNumber:
-        print("Your guess is right.")
-        break
-    else:
+    if guessNumber != randomNumber:
         print("Your guess is not right.")
+        x = x + 1
 
+    else:
+        x = x + 1
+        print("Your guess is right. You took total ",x,  "attempts")
+        break
 
